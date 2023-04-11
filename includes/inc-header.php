5efcf20f96a1f0bf0ba5fcfda60fb003589b9c1b
pieroangulocubas <pieroangulocubas.17@gmail.com>
2023-04-11 14:41:45 +0200

    Creación de carpetas, maquetación y segmentación de las maquetación

---
+++ includes/inc-header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Primer archivo</title>
+  <!-- Boostrap -->
+  <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+  <!-- Font awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- waitme -->
+  <link rel="stylesheet" href="./assets/plugins/waitme/waitMe.min.css">
+</head>
---
+++ views/view-cart.php
@@ -0,0 +1,230 @@
+<?php require_once 'includes/inc-header.php' ?>
+<body>
+<?php require_once 'includes/inc-navbar.php' ?>
+<div class="container-fluid py-5">
+  <!-- page content -->
+  <div class="row">
+    <div class="col-xl-8">
+      <h2>Productos</h2>
+      <div class="row">
+        <div class="col-md-3 mb-3">
+          <div class="card">
+            <img src="https://placeimg.com/300/300/tech" class="card-img-top" alt="...">
+            <div class="card-body p-2 ">
+              <h5 class="card-title text-truncate">Producto lorem ipsus</h5>
+              <button class="btn btn-sm btn-success" data-bs-toggle="tooltip" data-bs-title="Agregar al carrito">
+                <i class="fas fa-plus"></i>
+                Agregar al carrito
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 mb-3">
+          <div class="card">
+            <img src="https://placeimg.com/300/300/tech" class="card-img-top" alt="...">
+            <div class="card-body p-2 ">
+              <h5 class="card-title text-truncate">Producto lorem ipsus</h5>
+              <button class="btn btn-sm btn-success" data-bs-toggle="tooltip" data-bs-title="Agregar al carrito">
+                <i class="fas fa-plus"></i>
+                Agregar al carrito
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 mb-3">
+          <div class="card">
+            <img src="https://placeimg.com/300/300/tech" class="card-img-top" alt="...">
+            <div class="card-body p-2 ">
+              <h5 class="card-title text-truncate">Producto lorem ipsus</h5>
+              <button class="btn btn-sm btn-success" data-bs-toggle="tooltip" data-bs-title="Agregar al carrito">
+                <i class="fas fa-plus"></i>
+                Agregar al carrito
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 mb-3">
+          <div class="card">
+            <img src="https://placeimg.com/300/300/tech" class="card-img-top" alt="...">
+            <div class="card-body p-2 ">
+              <h5 class="card-title text-truncate">Producto lorem ipsus</h5>
+              <button class="btn btn-sm btn-success" data-bs-toggle="tooltip" data-bs-title="Agregar al carrito">
+                <i class="fas fa-plus"></i>
+                Agregar al carrito
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 mb-3">
+          <div class="card">
+            <img src="https://placeimg.com/300/300/tech" class="card-img-top" alt="...">
+            <div class="card-body p-2 ">
+              <h5 class="card-title text-truncate">Producto lorem ipsus</h5>
+              <button class="btn btn-sm btn-success" data-bs-toggle="tooltip" data-bs-title="Agregar al carrito">
+                <i class="fas fa-plus"></i>
+                Agregar al carrito
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 mb-3">
+          <div class="card">
+            <img src="https://placeimg.com/300/300/tech" class="card-img-top" alt="...">
+            <div class="card-body p-2 ">
+              <h5 class="card-title text-truncate">Producto lorem ipsus</h5>
+              <button class="btn btn-sm btn-success" data-bs-toggle="tooltip" data-bs-title="Agregar al carrito">
+                <i class="fas fa-plus"></i>
+                Agregar al carrito
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 mb-3">
+          <div class="card">
+            <img src="https://placeimg.com/300/300/tech" class="card-img-top" alt="...">
+            <div class="card-body p-2 ">
+              <h5 class="card-title text-truncate">Producto lorem ipsus</h5>
+              <button class="btn btn-sm btn-success" data-bs-toggle="tooltip" data-bs-title="Agregar al carrito">
+                <i class="fas fa-plus"></i>
+                Agregar al carrito
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 mb-3">
+          <div class="card">
+            <img src="https://placeimg.com/300/300/tech" class="card-img-top" alt="...">
+            <div class="card-body p-2 ">
+              <h5 class="card-title text-truncate">Producto lorem ipsus</h5>
+              <button class="btn btn-sm btn-success" data-bs-toggle="tooltip" data-bs-title="Agregar al carrito">
+                <i class="fas fa-plus"></i>
+                Agregar al carrito
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 mb-3">
+          <div class="card">
+            <img src="https://placeimg.com/300/300/tech" class="card-img-top" alt="...">
+            <div class="card-body p-2 ">
+              <h5 class="card-title text-truncate">Producto lorem ipsus</h5>
+              <button class="btn btn-sm btn-success" data-bs-toggle="tooltip" data-bs-title="Agregar al carrito">
+                <i class="fas fa-plus"></i>
+                Agregar al carrito
+              </button>
+            </div>
+          </div>
+        </div>
+     
+      </div>
+    </div>
+    <div class="col-xl-4">
+      <h2>Carrito</h2>
+      <div class="table-responsive">
+        <table class="table table-hover table-striped table-sm">
+          <thead>
+            <tr class="table-dark">
+              <th>Producto</th>
+              <th >Cantidad</th>
+              <th class="text-center">Total</th>
+              <th class="text-end"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="align-middle">
+                Producto 1
+                <small class="d-block text-muted">SKU 123456789</small>
+              </td>
+              <td class="align-middle" width="10%">
+                <input class="form-control form-control-sm" min=0 max=50 type="number" value=1>
+              </td>
+              <td class="text-center align-middle">$149.90</td>
+              <td class="text-end align-middle">
+                <i class="fas fa-trash text-danger"></i>
+              </td>
+            </tr>
+            <tr>
+              <td class="align-middle">
+                Producto 1
+                <small class="d-block text-muted">SKU 123456789</small>
+              </td>
+              <td class="align-middle" width="10%">
+                <input class="form-control form-control-sm" min=0 max=50 type="number" value=1>
+              </td>
+              <td class="text-center align-middle">$149.90</td>
+              <td class="text-end align-middle">
+                <i class="fas fa-trash text-danger"></i>
+              </td>
+            </tr>
+            <tr>
+              <td class="align-middle">
+                Producto 1
+                <small class="d-block text-muted">SKU 123456789</small>
+              </td>
+              <td class="align-middle" width="10%">
+                <input class="form-control form-control-sm" min=0 max=50 type="number" value=1>
+              </td>
+              <td class="text-center align-middle">$149.90</td>
+              <td class="text-end align-middle">
+                <i class="fas fa-trash text-danger"></i>
+              </td>
+            </tr>
+            <tr>
+              <td class="align-middle">
+                Producto 1
+                <small class="d-block text-muted">SKU 123456789</small>
+              </td>
+              <td class="align-middle" width="10%">
+                <input class="form-control form-control-sm" min=0 max=50 type="number" value=1>
+              </td>
+              <td class="text-center align-middle">$149.90</td>
+              <td class="text-end align-middle">
+                <i class="fas fa-trash text-danger"></i>
+              </td>
+            </tr>
+            <tr>
+              <td class="align-middle">
+                Producto 1
+                <small class="d-block text-muted">SKU 123456789</small>
+              </td>
+              <td class="align-middle" width="10%">
+                <input class="form-control form-control-sm" min=0 max=50 type="number" value=1>
+              </td>
+              <td class="text-center align-middle">$149.90</td>
+              <td class="text-end align-middle">
+                <i class="fas fa-trash text-danger"></i>
+              </td>
+            </tr>
+           
+          </tbody>
+        </table>
+      </div>
+      <button class="btn btn-danger">Vaciar carrito</button>
+      <!-- Totals table -->
+      <table class="table">
+        <tr>
+          <th  class="align-middle">Subtotal</th>
+          <td class="text-end">$250.50</td>
+        </tr>
+        <tr>
+          <th  class="align-middle">Envio</th>
+          <td class="text-end text-danger">$50.00</td>
+        </tr>
+        <tr>
+          <th class="align-middle">Total</th>
+          <td class="text-end text-xl align-middle">
+            <h3 class="fw-bold text-success">$200.50</h3>
+          </td>
+        </tr>
+      </table>
+
+      <!-- Pay now button -->
+      <div class="d-grid">    
+        <button class="btn btn-info btn-lg btn-block">Pagar ahora</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php require_once 'includes/inc-footer.php' ?>
